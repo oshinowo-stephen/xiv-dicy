b6873c4de619a5250a8b305d6fdbed63e1486981
oshinowo-stephen <kingstonicthe1@gmail.com>
2023-01-18 10:59:50 +0000

    unwanted mistake. also dist built

--- packages/verfication/dist/index.d.ts
+++ packages/verfication/dist/index.d.ts
@@ -1,10 +1,1 @@
-export interface VerifyCache {
-    cache: Cache;
-    duration?: number;
-}
-export interface Cache {
-    bio: string;
-    character: string;
-}
-export declare const vCache: VerifyCache[];
 export declare const verifyCharacter: (_key: string, charURI: string) => Promise<void>;
--- packages/verfication/dist/index.js
+++ packages/verfication/dist/index.js
@@ -3,31 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.verifyCharacter = exports.vCache = void 0;
+exports.verifyCharacter = void 0;
 const node_fetch_1 = __importDefault(require("node-fetch"));
 const cheerio_1 = require("cheerio");
-exports.vCache = [];
 const verifyCharacter = async (_key, charURI) => {
-    const characterCache = exports.vCache.find(({ cache }) => cache.character === charURI);
-    if (characterCache.duration > Date.now()) {
-        const response = await (0, node_fetch_1.default)(charURI);
-        const page = await response.text();
-        const $ = (0, cheerio_1.load)(page);
-        const charBio = $.html('.character__selfintroduction');
-        const bio = $(charBio).text();
-        exports.vCache.push({
-            cache: {
-                bio,
-                character: charURI,
-            },
-            duration: Date.now() + 5 * 1000
-        });
-        if (!bio.includes(_key)) {
-            throw new Error('Invalid Key.');
-        }
-    }
-    else {
-        throw new Error('This character is in the cache!');
+    const response = await (0, node_fetch_1.default)(charURI);
+    const page = await response.text();
+    const $ = (0, cheerio_1.load)(page);
+    const charBio = $.html('.character__selfintroduction');
+    const bio = $(charBio).text();
+    if (!bio.includes(_key)) {
+        throw new Error('Invalid Key.');
     }
 };
 exports.verifyCharacter = verifyCharacter;
