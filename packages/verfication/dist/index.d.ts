28e3788ec91c5934a8a0bfe3b5b0a9742ab9d66b
oshinowo-stephen <kingstonicthe1@gmail.com>
2023-01-21 10:31:21 +0000

    minor version update & dist

--- packages/verfication/dist/index.d.ts
+++ packages/verfication/dist/index.d.ts
@@ -1,1 +1,5 @@
-export declare const verifyCharacter: (_key: string, charURI: string) => Promise<void>;
+export interface Character {
+    name: string;
+    world: string;
+}
+export declare const verifyCharacter: (_key: string, charURI: string) => Promise<Character>;
--- packages/verfication/dist/index.js
+++ packages/verfication/dist/index.js
@@ -11,9 +11,13 @@ const verifyCharacter = async (_key, charURI) => {
     const page = await response.text();
     const $ = (0, cheerio_1.load)(page);
     const charBio = $.html('.character__selfintroduction');
-    const bio = $(charBio).text();
-    if (!bio.includes(_key)) {
+    const charName = $.html('.frame__chara__name');
+    const charWorld = $.html('.frame__chara__world');
+    if (!$(charBio).text().includes(_key))
         throw new Error('Invalid Key.');
-    }
+    return {
+        name: $(charName).text(),
+        world: $(charWorld).text()
+    };
 };
 exports.verifyCharacter = verifyCharacter;
